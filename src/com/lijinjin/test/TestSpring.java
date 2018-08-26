ae0b00f8cea8f66fa9da7118e895f01661e4dcd7
lijjai <lijjai@dcits.com>
2018-08-26 17:35:53 +0800

    springTest

---
+++ src/com/lijinjin/test/TestSpring.java
@@ -0,0 +1,23 @@
+package com.lijinjin.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.lijinjin.pojo.Category;
+import com.lijinjin.pojo.Product;
+
+public class TestSpring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String string="applicationContext.xml";
+		ApplicationContext applicationContext=new ClassPathXmlApplicationContext(string);
+		Category category=(Category) applicationContext.getBean("c");
+		System.out.println(category.getName());
+		
+		Product product=(Product) applicationContext.getBean("p");
+		System.out.println(product.getName());
+		System.out.println(product.getCategory().getName());
+	}
+
+}
